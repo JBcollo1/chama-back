bd9f3509db262a41fcb06cfef1a34074ce2c6d5a
Jorbat Collins <jbcollins254@gmail.com>
2025-08-20 10:29:25 +0300

    increase sccess token expiry time

--- auth/auth_service.py
+++ auth/auth_service.py
@@ -40,7 +40,7 @@ class AuthService:
         if expires_delta:
             expire = datetime.utcnow() + expires_delta
         else:
-            expire = datetime.utcnow() + timedelta(minutes=15)
+            expire = datetime.utcnow() + timedelta(hours=24)
         
         to_encode.update({
             "exp": expire,
