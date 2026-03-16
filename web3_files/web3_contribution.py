c335b1b7d61410ede07eec0eb175605adc76a4bf
Collins <jbcollins254@gmail.com>
2026-03-16 22:32:18 +0300

    contribution service write

--- web3_files/web3_contribution.py
+++ web3_files/web3_contribution.py
@@ -3,7 +3,7 @@ from typing import Optional
 from uuid import UUID
 
 from fastapi import HTTPException
-from web3 import web3
+from web3 import Web3
 from web3.exception import ContractLogicError
 
 from .web3_main import Web3Service
@@ -17,3 +17,12 @@ class ContributionContractService:
 
 
     # Helper function
+    def _get_group_contract(self,group_contract_address: str):
+        checksum_address = Web3.to_checksum_address(group_contract_address)
+        return self.web3.w3.eth.contract(
+            address = checksum_address,
+            abi = self.web3.group_abi,
+        )
+
+    def _build_and_send(self, fn, extra_value: int = 0) -> str:
+        self.
